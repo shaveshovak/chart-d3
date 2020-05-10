c3ae92686c2819f45cb2332359ae5da909f144ba
Khatuna <9b9de9fa>
2020-05-10 13:12:49 +0400

    ad pie chart and colours

--- graph.js
+++ graph.js
@@ -1,47 +1,76 @@
-const dims = { height: 300, width: 300, radius: 150};
-const cent = {x: (dims.width / 2 + 5), y: (dims.height / 2 + 5)};
+const dims = { height: 300, width: 300, radius: 150 };
+const cent = { x: (dims.width / 2 + 5), y: (dims.height / 2 + 5)};
+
+// create svg container
 const svg = d3.select('.canvas')
-    .append('svg')
-    .attr('width', dims.width + 150)
-    .attr('height', dims.height + 150);
-    
+	.append('svg')
+	.attr('width', dims.width + 150)
+	.attr('height', dims.height + 150);
+
 const graph = svg.append('g')
-    .attr('transform', `translate(${cent.x}, ${cent.y})`);
+  	.attr("transform", `translate(${cent.x}, ${cent.y})`);
+  	// translates the graph group to the middle of the svg container
 
 const pie = d3.pie()
-    .sort(null)
-    .value(d => d.cost);
-// the value we are evaluating to create the pie angles
+  	.sort(null)
+  	.value(d => d.cost);
+  	// the value we are evaluating to create the pie angles
 
-// console.log(angles);
 const arcPath = d3.arc()
-    .outerRadius(dims.radius)
-    .innerRadius(dims.radius / 2);
+  	.outerRadius(dims.radius)
+  	.innerRadius(dims.radius / 2);
+
+// ordianl colour scale
+const colour = d3.scaleOrdinal(d3["schemeSet3"]);
+
+// update function
+const update = (data) => {
+
+  	// update colour scale domain
+  	colour.domain(data.map(d => d.name));
+  
+  	// join enhanced (pie) data to path elements
+  	const paths = graph.selectAll('path')
+    	.data(pie(data));
+
+  	// handle the exit selection 
+
+  	// handle the current DOM path updates
+  	paths.enter()
+    	.append('path')
+      	.attr('class', 'arc')
+      	.attr('d', arcPath)
+      	.attr('stroke', '#fff')
+      	.attr('stroke-width', 3)
+      	.attr('fill', d => colour(d.data.name));
+
+};
 
 // data array and firestore
-
 var data = [];
 
-db.collection('expenses').onSnapshot(res => {
-    res.docChanges().forEach(change => {
-        const doc = {...change.doc.data(), id: change.doc.id}
-        switch(change.type) {
-            case 'added':
-                data.push(doc);
-                break;
-            case 'modified':
-                const index = data.findIndex(item => item.id == doc.id);
-                data[index] = doc;
-                break;
-            case 'removed':
-                data = data.filter(item => item.id !== doc.id);
-                break;
-            default:
-                break;
-        }
-    });
+db.collection('expenses').orderBy('cost').onSnapshot(res => {
 
-    update();
-})
+	res.docChanges().forEach(change => {
 
-// console.log(arcPath(angles[1]))
+    const doc = {...change.doc.data(), id: change.doc.id};
+
+    switch (change.type) {
+      	case 'added':
+        	data.push(doc);
+        break;
+      	case 'modified':
+        	const index = data.findIndex(item => item.id == doc.id);
+        	data[index] = doc;
+        break;
+      	case 'removed':
+        	data = data.filter(item => item.id !== doc.id);
+        break;
+      	default:
+        	break;
+    }
+});
+
+	// call the update function
+  	update(data);
+});
